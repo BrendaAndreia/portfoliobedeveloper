dd9e7ed46760e4341802eae502f70d2582a96017
brendaandreia <brendaandreia@gmail.com>
2023-09-22 21:31:31 -0300

    Responsividade - Home e Sobre

--- js/script.js
+++ js/script.js
@@ -1,13 +1,48 @@
-function navigateTo(section) {
-    document.getElementById('Home').style.display = 'none';
-    document.getElementById('Sobre').style.display = 'none';
-    document.getElementById('Projetos').style.display = 'none';
-    document.getElementById('Contatos').style.display = 'none';
-   
-    const targetSection = document.getElementById(section);
-    if (targetSection) {
-        targetSection.style.display = 'flex';
-        document.title = `${section} | Bê Developer`;
-     }
+function isSmallScreen() {
+  return window.matchMedia("(max-width: 767px)").matches;
 }
+
+function navigateTo(section) {
+  document.getElementById('Home').style.display = 'none';
+  document.getElementById('Sobre').style.display = 'none';
+  document.getElementById('Projetos').style.display = 'none';
+  document.getElementById('Contatos').style.display = 'none';
+  
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const windowDiv = document.getElementById("window");
+  const targetSection = document.getElementById(section);
+  const home = document.getElementById("Home")
+
+  if (targetSection) {
+    targetSection.style.display = 'flex';
+    document.title = `${section} | Bê Developer`;
+  }
+  
+  if (isSmallScreen()) {
+    if (section === 'Sobre' || section === 'Projetos') {
+      windowDiv.style.display = "none";
+      hamburgerButton.style.display = "block";
+
+    }
+    if (section === 'Home' || section === 'Contatos') {
+      hamburgerButton.style.display = "none";
+      home.style.height = "50vh";
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const windowDiv = document.getElementById("window");
+  
+  hamburgerButton.addEventListener("click", function() {
+    if (isSmallScreen()) {
+      if (windowDiv.style.display === "none" || windowDiv.style.display === "") {
+        windowDiv.style.display = "flex"; 
+      } else {
+        windowDiv.style.display = "none";
+      }
+    }
+  });
+});
 
